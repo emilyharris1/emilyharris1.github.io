5ad58f6b389b0cfc9999f679557240b43d454f4b
emilyharris1 <emily.harris-2@colorado.edu>
2023-11-16 13:59:34 -0700

    push

--- ica/ica11.2/ica11.2.js
+++ ica/ica11.2/ica11.2.js
@@ -1,20 +1,15 @@
 const btn = document.querySelector("button");
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector("body");
 
 function random(number){
     return Math.floor(Math.random() * number);
 }
 
 function draw(){
-    ctx.clearRect(0,0,canvas.width, canvas.height);
     let red = random(255);
     let color = "rgb("+red+"+0+0)";
-    document.body.style.background = color;
-    window.addEventListener("load",function() { changeBackground(color) });
+    canvas.style.backgroundColor = color;
+    window.addEventListener("load",function() {changeBackground(color)});
 }
 
 btn.addEventListener("click", draw);
-
-
-
--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -1,3 +1,21 @@
+const btn = document.querySelector("button");
+const canvas = document.querySelector("body");
+
+function random(number){
+    return Math.floor(Math.random() * number);
+}
+
+function draw(){
+    let red = random(255);
+    let color = "rgb("+red+"+0+0)";
+    canvas.style.backgroundColor = color;
+    window.addEventListener("load",function() {changeBackground(color)});
+}
+
+btn.addEventListener("click", draw);
+
+
+
 const newBtn = document.querySelector('#js-new-quote'). addEventListener('click', getQuestion);
 const newBtn2 = document.querySelector('#js-tweet'). addEventListener('click', displayPunchline);
 const body = document.querySelector('body');
