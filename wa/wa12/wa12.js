da037a0d1079ebf587cae18537caed558c745a60
emilyharris1 <emily.harris-2@colorado.edu>
2023-11-19 19:56:20 -0700

    push

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -6,8 +6,8 @@ function random(number){
 }
 
 function draw(){
-    let red = random(255);
-    let color = "rgb("+red+"+0+0)";
+    let green = random(255);
+    let color = "rgb(0+"+green+"+0)";
     canvas.style.backgroundColor = color;
     window.addEventListener("load",function() {changeBackground(color)});
 }
@@ -15,16 +15,19 @@ function draw(){
 btn.addEventListener("click", draw);
 
 
-
 const newBtn = document.querySelector('#js-new-quote'). addEventListener('click', getQuestion);
 const newBtn2 = document.querySelector('#js-tweet'). addEventListener('click', displayPunchline);
 const body = document.querySelector('body');
+
+const dogImg = document.querySelector('#js-dog-img');
+//document.getElementById("textContent");
 
 const questionTxt = document.querySelector('#js-quote-text');
 let punchlineTxt = document.querySelector('#js-answer-text');
 let punchline = '';
 const endpoint = 'https://official-joke-api.appspot.com/random_joke';
-// const element = document.querySelector('.demo');
+const endpoint2 = 'https://dog.ceo/api/breeds/image/random';
+
 document.body.style.background = color;
 
 async function getQuestion(){
@@ -34,14 +37,6 @@ async function getQuestion(){
             throw Error(response.statusText);
         }
         const json = await response.json();
-        if(json['type'] = 'programming'){
-            let color = "rgba(0,0,255,0.5)";
-            // sample.style.color = 'red';
-        }else{
-            // element.style.backgroundColor = 'red';
-            let color = "rgba(0,0,255,0.5)";
-            window.addEventListener("load",function() { changeBackground(color) });
-        }
         displayQuestion(json['setup']);
         punchline = json['punchline'];
         punchlineTxt.textContent = '';
@@ -50,10 +45,23 @@ async function getQuestion(){
         console.log(err);
         alert('Failed');
     }
+    // try{
+    //     const response2 = await fetch(endpoint2);
+    //     if(!response2.ok){
+    //         throw Error(response2.statusText);
+    //     }
+    //     const json2 = await response2.json2();
+    //     displayQuestion(json2['message']);
+    // }
+    // catch(err){
+    //     console.log(err);
+    //     alert('Failed');
+    // }
 }
 
 function displayQuestion(setup){
     questionTxt.textContent = setup;
+    //dogImg.imgContent = message;
 }
 
 function displayPunchline(){
